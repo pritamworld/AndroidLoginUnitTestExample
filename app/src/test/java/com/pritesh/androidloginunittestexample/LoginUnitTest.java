e763a9055be4dc32559c7fa1a2453b973130031a
Pritesh Patel <pritesh.patel@adesa.com>
2019-03-05 12:25:50 -0500

    Add more test cases

--- app/src/test/java/com/pritesh/androidloginunittestexample/LoginUnitTest.java
+++ app/src/test/java/com/pritesh/androidloginunittestexample/LoginUnitTest.java
@@ -5,7 +5,12 @@ import android.content.Context;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.Iterator;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -49,4 +54,34 @@ public class LoginUnitTest
         // ...then the result should be the expected one.
         assertEquals(result, INVALID_STRING);
     }
+
+    // demonstrates the return of multiple values
+    @Test
+    public void testMoreThanOneReturnValue()  {
+        Iterator<String> i= mock(Iterator.class);
+        when(i.next()).thenReturn("Mockito").thenReturn("rocks");
+        String result= i.next()+" "+i.next();
+        //assert
+        assertEquals("Mockito rocks", result);
+    }
+
+    // this test demonstrates how to return values based on the input
+    @Test
+    public void testReturnValueDependentOnMethodParameter()  {
+        Comparable<String> c= mock(Comparable.class);
+        when(c.compareTo("Mockito")).thenReturn(1);
+        when(c.compareTo("Eclipse")).thenReturn(2);
+        //assert
+        assertEquals(1, c.compareTo("Mockito"));
+    }
+
+    // this test demonstrates how to return values independent of the input value
+    @Test
+    public void testReturnValueInDependentOnMethodParameter()  {
+        Comparable<Integer> c= mock(Comparable.class);
+        when(c.compareTo(anyInt())).thenReturn(-1);
+        //assert
+        assertEquals(-1, c.compareTo(9));
+    }
+
 }
